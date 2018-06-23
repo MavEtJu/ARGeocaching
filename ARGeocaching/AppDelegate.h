a74e8739b85a8f1d68a0f0065288fb2d0c95f0e8
Edwin Groothuis <edwin@mavetju.org>
2018-06-23 11:52:02 +1000

    Initial Commit

---
+++ ARGeocaching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARGeocaching
+//
+//  Created by Edwin Groothuis on 23/6/18.
+//  Copyright © 2018 Edwin Groothuis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARGeocaching/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARGeocaching
+//
+//  Created by Edwin Groothuis on 23/6/18.
+//  Copyright © 2018 Edwin Groothuis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
